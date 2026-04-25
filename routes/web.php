9a2af36824b836553e5fc479aed452db0490c1e0
kevin <kev.aubree@gmail.com>
2026-04-25 13:21:12 +0200

    feat: GET / returns items from DB
    Signed-off-by: kevin <kev.aubree@gmail.com>

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Models\Item;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return Item::orderBy('id')->get();
 });
 
 Route::get('/items', function () {
